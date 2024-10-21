bbc709fdf245789651581c1053523db2a680b4c5
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-21 22:05:05 +0530

    LinkedIn link fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ export const Footer = () => {
           <li>
             <a
               className="flex items-center justify-start hover:underline duration-300 gap-x-3"
-              href="www.linkedin.com/in/mohammad-arsalan-rather-27628722a"
+              href="www.linkedin.com/in/mohammad-arsalan-rather-27628722a" target="_blank"
             >
               <FaLinkedin size={20} />
               <p>www.linkedin.com/in/mohammad-arsalan-rather-27628722a</p>
@@ -34,7 +34,7 @@ export const Footer = () => {
           <li>
             <a
               className="flex items-center justify-start hover:underline duration-300 gap-x-3"
-              href="https://github.com/Arslanamin404"
+              href="https://github.com/Arslanamin404" target="_blank"
             >
               <FaGithub size={20} />
               <p>https://github.com/Arslanamin404</p>
