7359b372870669ac70471a79b44d34a9ca7334d1
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-25 08:46:45 +0530

    New Projectr added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,40 +1,26 @@
 import React from "react";
 import { ProjectCard } from "./ProjectCard";
-import TaxiService from "../../assets/TaxiService.png";
-import ExpenseTracker from "../../assets/ExpenseTracker.png";
-import PrTracker from "../../assets/PrTracker.png";
+import { projectsData } from "../../utils/projectData";
 
 export const Projects = () => {
   return (
-    <div id="Projects" className="p-10 md:px-24 md:py-14 text-white bg-black/30">
+    <div
+      id="Projects"
+      className="p-10 md:px-24 md:py-14 text-white bg-black/30"
+    >
       <h1 className="text-2xl md:text-4xl text-white uppercase font-extrabold text-center">
         Projects
       </h1>
       <div className="py-12 md:px-8 flex items-center justify-center flex-wrap gap-10 md:gap-16">
-        <ProjectCard
-          ImageSrc={TaxiService}
-          cardTitle={"Car Taxi Service Website"}
-          cardBody={
-            "A static website showcasing car taxi services, complete with a list of vehicles, pricing, and driver details. Responsive design with easy-to-modify components."
-          }
-          cardLink={"https://turbotaxis.netlify.app"}
-        />
-        <ProjectCard
-          ImageSrc={PrTracker}
-          cardTitle={"PR Tracker with Redux Toolkit"}
-          cardBody={
-            "A performance records tracker built using RTK to add, delete, and search exercise records. Includes state persistence using redux-persist."
-          }
-          cardLink={"https://strengthquest.netlify.app/"}
-        />
-        <ProjectCard
-          ImageSrc={ExpenseTracker}
-          cardTitle={"Expense Tracker with RTK"}
-          cardBody={
-            "A fully functional expense tracker app that helps users manage their daily expenses. It includes features like adding, editing, deleting, and searching transactions. Built using Redux Toolkit for efficient state management and integrated with redux-persist to retain data across sessions."
-          }
-          cardLink={"https://expensecanvas.netlify.app/"}
-        />
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            ImageSrc={project.imageSrc}
+            cardTitle={project.title}
+            cardBody={project.description}
+            cardLink={project.link}
+          />
+        ))}
       </div>
     </div>
   );
